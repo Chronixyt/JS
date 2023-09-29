9b1a1aba88498151533108dab198ab341d50d157
Chronix <88084672+Chronixyt@users.noreply.github.com>
2023-09-29 09:23:10 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+// "use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// Datatypes
+
+let name = "Chronix" 
+let age = 21 
+let isLoggedIn = false 
+let state = null 
+// String => ""
+// number // bigint for large number
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null);
