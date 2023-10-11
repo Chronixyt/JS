ad57116c0dfe056d80b47c18537d666304de116a
Chronix <88084672+Chronixyt@users.noreply.github.com>
2023-10-11 04:41:11 +0000

    conversion operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,27 +1,35 @@
-let score = "prem"
+// let score = "prem"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof (valueInNumber));
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
 
-// "33" => 33
-// "33abd" => NaN (not a number)
-// true => 1; false => 0
+// // "33" => 33
+// // "33abd" => NaN (not a number)
+// // true => 1; false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false
-// "" => false
-// "prem" => true
+// // 1 => true; 0 => false
+// // "" => false
+// // "prem" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************* Oprations ********************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
